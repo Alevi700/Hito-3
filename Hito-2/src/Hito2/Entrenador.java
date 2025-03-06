593997921d0c9dbcef6c2f756521306f32feaaef
ruthf <ruthf@172.19.131.242>
2025-03-06 10:38:30 +0100

    ñ

--- Hito-2/src/Hito2/Entrenador.java
+++ Hito-2/src/Hito2/Entrenador.java
@@ -1,5 +1,11 @@
 package Hito2;
 
+
 public class Entrenador {
+	private String nombre;
+	private int [] pokeballs = new int [] {4,12,12,0,4,10};
+	Pokemon pokemons[] = new Pokemon [6];
+	int numeropokemons = 0;
+	int indicepokeballs = 0;
 
 }
